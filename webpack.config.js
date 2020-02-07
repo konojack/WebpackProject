112a8829168ffed5c84bef31f52bfd1e62624d44
Dawid Konojacki <dawid.konojacki@sb-betting.com>
2020-02-07 14:07:22 +0100

    Adding CommonsChunkPlugin && HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'faker', 'lodash', 'react', 'redux', 'react-redux', 'react-dom', 'react-input-range', 'redux-form', 'redux-thunk'
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
@@ -19,5 +27,15 @@ module.exports = {
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    // remove duplicates from bundle and place it only at vendor file
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    // choose template to generate scripts inside (our index.html with #root)
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
 };
